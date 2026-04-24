1c4d2850de856456205529cab009fda8b2cc2ec5
DannyDong <danield494@nycstudents.net>
2026-04-24 14:01:19 -0400

    1

--- Gacha/src/views/HomeView.vue
+++ Gacha/src/views/HomeView.vue
@@ -5,11 +5,19 @@
 </template>
 
 <script>
-  export default {
-    
-  }
+ import { ref, onMounted } from 'vue'
+ import { supabase } from 'src/views/supabase'
+  
+ const todos = ref([])
+ async function getTodos() {
+   const { data } = await supabase.from('todos').select()
+   todos.value = data
+ }
+onMounted(() => {
+  getTodos()
+})
 </script>
 
-<style lang="scss" scoped>
+<style>
 
 </style>
