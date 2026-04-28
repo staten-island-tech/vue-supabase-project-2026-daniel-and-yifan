2f1680e54925b6127d49e9e74257bfd2638da366
DannyDong <danield494@nycstudents.net>
2026-04-28 13:09:54 -0400

    1

--- Gacha/src/App.vue
+++ Gacha/src/App.vue
@@ -1,7 +1,34 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabase'
+
+const transactions = ref([])
+const error = ref(null)
+
+onMounted(async () => {
+
+  let { data: transactionData, error: err } = await supabase
+    .from('transactions')
+    .select('*')
+
+  if (err) {
+    error.value = err.message
+  } else {
+    transactions.value = transactionData
+  }
+})
+</script>
 
 <template>
-  <RouterView/>
+  <main>
+    <h1>Transaction Log</h1>
+    
+    <p v-if="error" style="color: red;">{{ error }}</p>
+    
+    <ul>
+      <li v-for="t in transactions" :key="t.id">
+        {{ t.created_at }}: {{ t.amount }}
+      </li>
+    </ul>
+  </main>
 </template>
-
-<style scoped></style>
