8595abdd8a7470f9b422462c7f5c524e2e2399c3
DannyDong <danield494@nycstudents.net>
2026-04-29 13:14:29 -0400

    1

--- Gacha/src/views/home.vue
+++ Gacha/src/views/home.vue
@@ -11,23 +11,6 @@ import router from '@/router';
 import { supabase } from '@/superbase';
 import { ref, onMounted } from 'vue';
 
-const collGachaItems = ref([])
-const error = ref(null)
-
-onMounted(async () => {
-  let { data: gachaItems, error: err } = await supabase
-    .from('gachaItems')
-    .select('*')
-  if (err) {
-    error.value = err.message
-  } else {
-    console.log(gachaItems)
-    collGachaItems.value = gachaItems
-  }
-})
-
-console.log(collGachaItems)
-
 
 </script>
 
