a2d3c2baa771afb05333f765c7a81f377ed6238b
YH4-ES <yifanh4@nycstudents.net>
2026-04-30 14:01:21 -0400

    xcvb

--- Gacha/src/views/gacha.vue
+++ Gacha/src/views/gacha.vue
@@ -1,29 +1,41 @@
 <template>
     <div>
         <h1>john</h1>
-        <h1 @click="">roll</h1>
+        <h1 @click="doAGachaRoll()">roll</h1>
     </div>
 </template>
 
 <script setup>
 
-import { supabase } from '@/superbase';
+import { supabase } from '@/supabase';
 import { ref, onMounted } from 'vue';
 
-const collGachaItems = ref([])
+const collGachaItems = ref({
+  horrible : [],
+  good : [],
+  yeah : [],
+  great : [],
+})
 const error = ref(null)
 
-onMounted(async () => {
+async function grabItems(select, equal) {
+  if (!select) {
+    select = "*"
+  }
+  if (!equal) {
+    equal = "*"
+  }
   let { data: gachaItems, error: err } = await supabase
     .from('gachaItems')
-    .select('*')
+    .select(select)
+    .eq("rarity", equal)
   if (err) {
     error.value = err.message
   } else {
     console.log(gachaItems)
     collGachaItems.value = gachaItems
   }
-})
+}
 
 function roll(min, max){
   let random = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -31,7 +43,17 @@ function roll(min, max){
 }
 
 function doAGachaRoll(){
-  
+  let rngRoll = roll(1, 100)
+  if (rngRoll < 40) {
+    let items = grabItems(undefined, "horrible")
+    console.log("horrible", items)
+  } else if (rngRoll < 70) {
+    console.log("Good enough")
+  } else if (rngRoll < 90) {
+    console.log("Yeah")
+  } else {
+    console.log("Great")
+  }
 }
 
 
