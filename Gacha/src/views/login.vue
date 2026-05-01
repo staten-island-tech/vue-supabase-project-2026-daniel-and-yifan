d96bc6d6817e9f893b8db4d5765ca058a6407c2d
DannyDong <danield494@nycstudents.net>
2026-05-01 13:03:22 -0400

    1

--- Gacha/src/views/login.vue
+++ Gacha/src/views/login.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <form>
-          
-        </form>
+      <form @submit.prevent="handleLogin">
+        <input v-model="email" type="email" placeholder="Enter your email"/>
+      <button type="submit">Login</button>
+    </form>
     </div>
 </template>
 
@@ -13,16 +14,19 @@ import { ref, onMounted } from 'vue';
 const email = ref('')
 
 onMounted(async function handleLogin() {
-  const { data: profileInfo, error } = await supabase.from("profile").select("email")
+  const { data: profileInfo, error } = await supabase.from("profile").select("email").eq('email',email.value).single()
 
-  if (error) {
-    console.error('Error logging in:', error.message)
-  } else {
-    console.log('Logged in successfully:', profileInfo)
+    if (error || !data) {
+    console.error('Email not found')
+    return
   }
+
+  console.log('Login success:', data)
 })
 </script>
 
 <style lang="scss" scoped>
 
 </style>
+
+
