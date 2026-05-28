4b86a9ca754b99cefa24d16452f0be71072a58f3
DannyDong <danield494@nycstudents.net>
2026-05-28 14:02:14 -0400

    1

--- Gacha/src/views/enemies.vue
+++ Gacha/src/views/enemies.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="enemy in enemies" :key="'hp-' + enemy.id">HP: {{ enemy.hp }}</div>
-        <img v-for="enemy in enemies" :key="'img-' + enemy.id" src="./enemy/goblin.png" alt="goblin"@click="hitEnemy(enemy.id)"/>
+        <img v-for="enemy in enemies" :key="'img-' + enemy.id" src="/enemy/goblin.png" alt="goblin" @click="hitEnemy(enemy.id)"/>
     </div>
 </template>
 
@@ -9,21 +9,22 @@
 import { supabase } from '@/supabase'
 import { ref } from 'vue'
 
-
-const enemies = ref([
-  { id: 1, hp: 10 },
-  { id: 2, hp: 10 },
-  { id: 3, hp: 10 },
-]) 
+const enemies = ref([ 
+{ id: 1 , hp: 3 , maxHp: 3 , rarity: null , drops: null }, 
+{ id: 2 , hp: 5 , maxHp: 5 , rarity: null , drops: null }, 
+])
 
 function hitEnemy(enemyId) {
   const enemy = enemies.value.find(e => e.id === enemyId)
   if (!enemy) return
-
-  enemy.hp -= 1
-  if (enemy.hp <= 0) {
+   if (enemy.hp <= 0) {
     enemies.value = enemies.value.filter(e => e.id !== enemyId)
   }
+  if  (enemy.hp <= 0) {
+    enemy.hp = enemy.maxHp
+  return
+  }
+  enemy.hp -= 1
 }
 </script>
 
