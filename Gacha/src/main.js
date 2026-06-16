f9e8970ae8f227be62b9e93ff81e45e868c4edb2
Daniel <danield494@nycstudents.net>
2026-06-15 22:18:47 -0400

    1

--- Gacha/src/main.js
+++ Gacha/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import "@/style.css"
+import "./style.css"
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
